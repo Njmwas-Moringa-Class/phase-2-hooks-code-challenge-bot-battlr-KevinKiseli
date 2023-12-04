461dd04604fa158e0797caabef7e6808e942414c
Kevin <kevin.kiseli@student.moringaschool.com>
2023-12-04 21:14:43 +0300

    Done

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,15 +1,80 @@
+
+
 import React from "react";
 
-function BotCollection() {
-  // Your code here
+const BotCollection = ({ bots, enlistBot }) => {
+
+  //Bot styling
+  
+  const containerStyle = {
+    maxWidth: "800px",
+    margin: "20px auto",
+    textAlign: "center",
+    color: "#2c3e50",
+  };
+
+  const botListStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+    gap: "20px",
+    justifyContent: "center",
+  };
+
+  const botItemStyle = {
+    width: "100%",
+    padding: "15px",
+    border: "2px solid #3498db",
+    borderRadius: "10px",
+    boxSizing: "border-box",
+    cursor: "pointer",
+    transition: "transform 0.3s ease-in-out",
+    backgroundColor: "#ecf0f1",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+  };
+
+  const handleHover = (e) => {
+    e.currentTarget.style.transform = "scale(1.05)";
+  };
+
+  const handleHoverOut = (e) => {
+    e.currentTarget.style.transform = "scale(1)";
+  };
+
+  // The Bot Attributes
+
   return (
-    <div className="ui four column grid">
-      <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
+    <div style={containerStyle}>
+      <h1>My Bot Collection</h1>
+      <div style={botListStyle}>
+        {bots.map((bot) => (
+          <div
+            key={bot.id}
+            style={botItemStyle}
+            onClick={() => enlistBot(bot)}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleHoverOut}
+          >
+            <h2 style={{ color: "#e74c3c" }}>{bot.name}</h2>
+            <p>ID: {bot.id}</p>
+            <p>💚 Health: {bot.health}</p>
+            <p>💥 Damage: {bot.damage}</p>
+            <p>🛡️ Armor: {bot.armor}</p>
+            <p>🤖 Class: {bot.bot_class}</p>
+            <p>🔮 Catchphrase: "{bot.catchphrase}"</p>
+            <img
+              src={bot.avatar_url}
+              alt={`${bot.name} Avatar`}
+              style={{
+                maxWidth: "100%",
+                borderRadius: "5px",
+                marginTop: "10px",
+              }}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default BotCollection;
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,16 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
-  //start here with your code for step one
+  // State for all available bots
+  const [bots, setBots] = useState([]);
 
+  //State for bots in my army
+  const [enlistedBots, setEnlistedBots] = useState([]);
+
+
+  // Fetches bots from API
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+      .then((response) => response.json())
+      .then((bots) => {
+        setBots(bots);
+      });
+  }, []);
+
+  // Enlists bots in army
+  const enlistBot = (bot) => {
+    
+    // checking if bot is already enlisted
+    if (!enlistedBots.some((enlistedBot) => enlistedBot.id === bot.id)) {
+
+      // Add a bot
+      setEnlistedBots((prevEnlistedBots) => [
+        ...prevEnlistedBots,
+        {
+          id: bot.id,
+          name: bot.name,
+          health: bot.health,
+          damage: bot.damage,
+          armor: bot.armor,
+          bot_class: bot.bot_class,
+          catchphrase: bot.catchphrase,
+          avatar_url: bot.avatar_url,
+        },
+      ]);
+    }
+  };
+
+
+  //Remove bot from army
+  const removeFromArmy = (botId) => {
+
+    //Filters bots
+    setEnlistedBots((prevEnlistedBots) =>
+      prevEnlistedBots.filter((bot) => bot.id !== botId)
+    );
+  };
+
+  const releaseBot = (botId) => {
+    // This deletes the bot from the API backend
+    fetch(`http://localhost:8002/bots/${botId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to delete bot from the backend");
+        }
+      })
+    
+      // .catch((error) => {
+      //   console.error(error);
+      // });
+
+    // Release the bot from the army
+    removeFromArmy(botId);
+  };
+
+
+  // BotsPage format
   return (
-    <div>
-      <YourBotArmy />
-      <BotCollection />
-    </div>
-  )
+    
+    <div style={{ fontFamily: "Arial, sans-serif", textAlign: "center" }}>
+      <h1>My Bot Army</h1>
+      <YourBotArmy
+        enlistedBots={enlistedBots}
+        removeFromArmy={removeFromArmy}
+        releaseBot={releaseBot}
+      />
+      <BotCollection bots={bots} enlistBot={enlistBot} />
+      <p style={{ marginTop: "20px", fontStyle: "italic" }}>
+        Ready For War
+      </p>
+      </div>
+  );
 }
 
 export default BotsPage;
